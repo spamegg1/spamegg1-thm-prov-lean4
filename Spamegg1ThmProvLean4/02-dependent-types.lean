03018883e5b5ceab1ec983a61107ec1f88213709
spamegg <spamegg@pm.me>
2024-11-06 22:14:54 +0300

    continue dependent types

---
+++ Spamegg1ThmProvLean4/02-dependent-types.lean
@@ -0,0 +1,102 @@
+/- Define some constants. -/
+
+def m : Nat := 1       -- m is a natural number
+def n : Nat := 0
+def b1 : Bool := true  -- b1 is a Boolean
+def b2 : Bool := false
+
+/- Check their types. -/
+
+#check m            -- output: Nat
+#check n
+#check n + 0        -- Nat
+#check m * (n + 0)  -- Nat
+#check b1           -- Bool
+#check b1 && b2     -- "&&" is the Boolean and
+#check b1 || b2     -- Boolean or
+#check true         -- Boolean "true"
+
+/- Evaluate -/
+
+#eval 5 * 4         -- 20
+#eval m + 2         -- 3
+#eval b1 && b2      -- false
+
+#check Nat → Nat      -- type the arrow as "\to" or "\r"
+#check Nat -> Nat     -- alternative ASCII notation
+
+#check Nat × Nat      -- type the product as "\times"
+#check Prod Nat Nat   -- alternative notation
+
+#check Nat → Nat → Nat
+#check Nat → (Nat → Nat)  --  same type as above
+
+#check Nat × Nat → Nat
+#check (Nat → Nat) → Nat -- a "functional"
+
+#check Nat.succ     -- Nat → Nat
+#check (0, 1)       -- Nat × Nat
+#check Nat.add      -- Nat → Nat → Nat
+
+#check Nat.succ 2   -- Nat
+#check Nat.add 3    -- Nat → Nat
+#check Nat.add 5 2  -- Nat
+#check (5, 9).1     -- Nat
+#check (5, 9).2     -- Nat
+
+#eval Nat.succ 2   -- 3
+#eval Nat.add 5 2  -- 7
+#eval (5, 9).1     -- 5
+#eval (5, 9).2     -- 9
+
+-- Types as objects
+#check Nat               -- Type
+#check Bool              -- Type
+#check Nat → Bool        -- Type
+#check Nat × Bool        -- Type
+#check Nat → Nat         -- ...
+#check Nat × Nat → Nat
+#check Nat → Nat → Nat
+#check Nat → (Nat → Nat)
+#check Nat → Nat → Bool
+#check (Nat → Nat) → Nat
+
+def α : Type := Nat
+def β : Type := Bool
+def F : Type → Type := List
+def G : Type → Type → Type := Prod
+
+#check α        -- Type
+#check F α      -- Type
+#check F Nat    -- Type
+#check G α      -- Type → Type
+#check G α β    -- Type
+#check G α Nat  -- Type
+
+#check Prod α β       -- Type
+#check α × β          -- Type
+
+#check Prod Nat Nat   -- Type
+#check Nat × Nat      -- Type
+
+#check List α    -- Type
+#check List Nat  -- Type
+
+#check Type     -- Type 1
+#check Type 1   -- Type 2
+#check Type 2   -- Type 3
+#check Type 3   -- Type 4
+#check Type 4   -- Type 5
+
+#check List    -- List.{u} (α : Type u) : Type u
+#check Prod    -- Prod.{u, v} (α : Type u) (β : Type v) : Type (max u v)
+
+-- universe u
+def H.{u} (α : Type u) : Type u := Prod α α
+#check H    -- Type u → Type u
+
+#check fun (x : Nat) => x + 5   -- Nat → Nat
+#check λ (x : Nat) => x + 5     -- λ and fun mean the same thing
+#check fun x => x + 5     -- Nat inferred
+#check λ x => x + 5       -- Nat inferred
+#eval (λ x : Nat => x + 5) 10    -- 15
